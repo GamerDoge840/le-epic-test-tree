451bad8975fd039917559155d1d45af78b057a66
GamerDoge840 <57807717+GamerDoge840@users.noreply.github.com>
2025-05-01 22:00:11 -0400

    a
    a

--- js/mod.js
+++ js/mod.js
@@ -2,8 +2,13 @@ let modInfo = {
 	name: "The Tree of Knowledge",
 	id: "superultraduperamogussmartspunchisus",
 	author: "The Big G",
+<<<<<<< Updated upstream
 	pointsName: "Knowledge",
 	modFiles: ["Layers/Row 0/Scrolls.js","Layers/Row 0/Knowledge.js","math.js", "Layers/Side/achievements.js", "tree.js"],
+=======
+	pointsName: "Essence",
+	modFiles: ["Layers/Row 2/Hindrance.js","Layers/Row 2/Glory.js","Layers/Row 1/Energy.js","Layers/Row 1/Rank.js","Layers/Row 1/Honor.js","Layers/Row 0/Level.js","Layers/Row 0/Prestige.js", "math.js", "Layers/Side/stats.js", "Layers/Side/achievements.js", "tree.js"],
+>>>>>>> Stashed changes
 
 	discordName: "",
 	discordLink: "",
